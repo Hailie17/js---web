b1fcbb95a29b25f4f20d47c9265445c224b7c561
Hailie17 <192151452@qq.com>
2024-10-22 20:13:15 +0800

    清除第一行有波浪线报错

--- yixiantong2/vue.config.js
+++ yixiantong2/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': __dirname + '/src',
+    }
+  }
 })
