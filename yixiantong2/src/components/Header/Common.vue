0c8d6398c3596847b2820f4a97a7f4cb671a1085
Hailie17 <192151452@qq.com>
2024-10-23 22:12:20 +0800

    城市选择组件

---
+++ yixiantong2/src/components/Header/Common.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'CommonHeader',
+  props: {
+    title: {
+      type: String,
+      default(){
+        return '一线生活通'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/mixins.scss';
+@import '@/assets/styles/variables.scss';
+
+</style>
--- yixiantong2/src/pages/City.vue
+++ yixiantong2/src/pages/City.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="container">City</div>
+  <div class="container">
+    <common-header :title="title"></common-header>
+  </div>
 </template>
 
-<script></script>
+<script>
+import CommonHeader from '@/components/Header/Common.vue'
+
+export default {
+  name: 'City',
+  components: {
+    CommonHeader
+  },
+  data(){
+    return {
+      title: '城市选择'
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
