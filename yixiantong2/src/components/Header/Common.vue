c1cc88518d94300b2bb0b6c66495dbf5e156c409
Hailie17 <192151452@qq.com>
2024-10-23 22:34:39 +0800

    返回组件

--- yixiantong2/src/components/Header/Common.vue
+++ yixiantong2/src/components/Header/Common.vue
@@ -1,12 +1,17 @@
 <template>
   <header class="header">
+    <back-ward></back-ward>
     <h1>{{ title }}</h1>
   </header>
 </template>
 
 <script>
+import BackWard from './Sub/BackWard.vue'
 export default {
   name: 'CommonHeader',
+  components: {
+    BackWard
+  },
   props: {
     title: {
       type: String,
@@ -21,5 +26,16 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/styles/mixins.scss';
 @import '@/assets/styles/variables.scss';
+
+.header {
+  position: relative;
+  @include flex-row;
+  @include vh-center;
+  height: $headerHeight;
+  background-color: $defaultBgColor;
+  h1 {
+    font-size: .18rem;
+  }
+}
 
 </style>
