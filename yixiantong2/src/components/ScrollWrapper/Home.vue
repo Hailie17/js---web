608b4b9f0123a7b8ce86dcd8fab47f4b347d01d9
Hailie17 <192151452@qq.com>
2024-10-27 00:30:40 +0800

    545 拿到数据

--- yixiantong2/src/components/ScrollWrapper/Home.vue
+++ yixiantong2/src/components/ScrollWrapper/Home.vue
@@ -10,13 +10,27 @@
 import BetterScroll from "better-scroll";
 import CategoryIcons from './CategoryIcons/Index.vue'
 import {mapState} from "vuex";
-import {IndexModel} from "@/modules";
+import {IndexModel} from "@/modules/index";
 
 export default {
   name: 'HomeScrollWrapper',
   components: {
     BetterScroll,
     CategoryIcons
+  },
+  computed: {
+    ...mapState(['cityId'])
+  },
+  mounted() {
+    this.getHomeDatas(this.cityId)
+  },
+  methods: {
+    getHomeDatas(cityId){
+      const indexModel = new IndexModel() // 实例化
+      indexModel.getHomeDatas(cityId).then((res)=> {
+        console.log(res)
+      })
+    }
   }
 }
 </script>
