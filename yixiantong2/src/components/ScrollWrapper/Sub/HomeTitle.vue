ed3f738c5a860240270ba2f10c249e3a135992e2
Hailie17 <192151452@qq.com>
2024-10-27 01:08:53 +0800

    888 routerlink 动态路由和参数

--- yixiantong2/src/components/ScrollWrapper/Sub/HomeTitle.vue
+++ yixiantong2/src/components/ScrollWrapper/Sub/HomeTitle.vue
@@ -18,4 +18,19 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-title {
+  display: flex;
+  align-items: center;
+  height: .5rem;
+  margin-top: .1rem;
+  padding-left: .15rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+  p {
+    font-size: .18rem;
+    color: #000;
+  }
+}
+</style>
