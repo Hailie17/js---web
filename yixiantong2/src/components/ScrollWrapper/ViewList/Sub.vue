e630e6ca78002b1395a4668e3384174cffceb753
Hailie17 <192151452@qq.com>
2024-10-27 01:26:53 +0800

    888 列表组件卡片

--- yixiantong2/src/components/ScrollWrapper/ViewList/Sub.vue
+++ yixiantong2/src/components/ScrollWrapper/ViewList/Sub.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="view-item">
-    <router-link :to="{path: '/detail', query: {field: 'view', id: data.id}}"></router-link>
+    <router-link :to="{path: '/detail', query: {field: 'view', id: data.id}}">
+      <div class="img">
+        <img :src="data.img" :alt="data.name" class="view-img">
+      </div>
+      <div class="info">
+        <h1 class="title">{{data.name}}</h1>
+        <p class="stars">
+          <span class="score">{{data.score}}</span>
+        </p>
+        <p class="others">
+          <span class="item">
+            <span class="price">￥ {{data.default_price}}</span>
+            <span class="item">{{data.city_name}}</span>
+          </span>
+        </p>
+      </div>
+    </router-link>
   </div>
 </template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
