31716e360a1a2a11d35e5bd3b6bfd452b1ad50ad
Hailie17 <192151452@qq.com>
2024-10-27 22:40:49 +0800

    799 美食列表 卡片

---
+++ yixiantong2/src/components/ScrollWrapper/FoodList/Sub.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="food-item">
+    <router-link :to="{path: '/detail', query: {field: 'food', id: data.id}}">
+      <div class="img">
+        <img :src="data.img" class="food-img" alt="data.name">
+      </div>
+      <div class="info">
+        <h1 class="title">{{data.name}}</h1>
+        <p class="stars">
+          <stars></stars>
+          <span class="price">￥ {{data.default_price}}/人</span>
+        </p>
+        <p class="keyword">
+          <span class="badge" v-for="item of data.keyword">
+            {{item}}
+          </span>
+        </p>
+        <p class="other">
+          <span class="item">{{data.field}}</span>
+          <span class="item">{{data.area}}</span>
+        </p>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Stars from "@/components/ScrollWrapper/Sub/Stars.vue";
+export default {
+  name: 'FoodListSub',
+  components: {
+    Stars
+  },
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style></style>
