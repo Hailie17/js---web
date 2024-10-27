087cfdaa42e6d368b126d444c0e93ef31997a670
Hailie17 <192151452@qq.com>
2024-10-27 23:05:50 +0800

    799 美食列表组件

---
+++ yixiantong2/src/components/ScrollWrapper/FoodList/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="list-wrapper">
+    <food-list-sub v-for="(item, index) of foodDatas" :data="item" :key="index"></food-list-sub>
+  </div>
+</template>
+
+<script>
+import FoodListSub from './Sub.vue'
+export default {
+  name: 'FoodList',
+  components: {
+    FoodListSub
+  }
+}
+</script>
