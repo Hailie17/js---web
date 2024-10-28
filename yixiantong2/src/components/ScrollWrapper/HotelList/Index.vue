e7dd889db9274bdbe542bcc0cda37191696c496e
Hailie17 <192151452@qq.com>
2024-10-28 21:58:26 +0800

    799 酒店卡片列表组件

---
+++ yixiantong2/src/components/ScrollWrapper/HotelList/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="list-wrapper">
+    <hotel-list-sub v-for="(item, index) of hotelDatas" :data="item" :key="index"></hotel-list-sub>
+  </div>
+</template>
+
+<script>
+import HotelListSub from './Sub.vue'
+export default {
+  name: 'HotelList',
+  components: {
+    HotelListSub
+  },
+  props: {
+    hotelDatas: Array
+  }
+}
+</script>>
