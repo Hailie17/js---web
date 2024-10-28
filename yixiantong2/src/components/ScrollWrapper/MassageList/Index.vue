721264e729c40746a8082fa5653cb98e890d6604
Hailie17 <192151452@qq.com>
2024-10-28 23:55:11 +0800

    445 按摩列表

---
+++ yixiantong2/src/components/ScrollWrapper/MassageList/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="list-wrapper">
+    <massage-list-sub v-for="(item,index) of massageDatas" :data="item" :key="index"></massage-list-sub>
+  </div>
+</template>
+
+<script>
+import MassageListSub from './Sub.vue'
+export default {
+  name: 'MassageList',
+  components: {
+    MassageListSub
+  },
+  props: {
+    massageDatas: Array
+  }
+}
+</script>
