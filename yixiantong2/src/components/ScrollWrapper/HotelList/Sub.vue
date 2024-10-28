9b9ff8d557b33a84f4734336cdca5ef3ecb932d9
Hailie17 <192151452@qq.com>
2024-10-28 23:08:06 +0800

    192 &:nth-child(1)样式

--- yixiantong2/src/components/ScrollWrapper/HotelList/Sub.vue
+++ yixiantong2/src/components/ScrollWrapper/HotelList/Sub.vue
@@ -28,4 +28,52 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/assets/styles/mixins.scss';
+@import '~@/assets/styles/variables.scss';
+.hotel-item {
+  position: relative;
+  border-bottom: 1px solid #ddd;
+  background-color: #fff;
+  .img {
+    width: 1.2rem;
+    height: 1.2rem;
+    padding: .1rem;
+    box-sizing: border-box;
+    .hotel-img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: .1rem .1rem 0 1.2rem;
+    box-sizing: border-box;
+    .title {
+      @include ellipsis;
+      font-size: .18rem;
+      color: #000;
+      line-height: .2rem;
+    }
+    .score {
+      @include ellipsis;
+      font-size: .14rem;
+      color: #999;
+    }
+    .others {
+      display: flex;
+      justify-content: space-between;
+      line-height: .25rem;
+      .item {
+        font-size: .14rem;
+        &:nth-child(1) {
+          color: $defaultGreen;
+        }
+      }
+    }
+  }
+}
+</style>
