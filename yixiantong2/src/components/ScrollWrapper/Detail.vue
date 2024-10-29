6d1922d0674ee97916c0361f245dc5eb45fd602b
Hailie17 <192151452@qq.com>
2024-10-29 23:59:14 +0800

    382 详情页数据模型

---
+++ yixiantong2/src/components/ScrollWrapper/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="scroll-wrapper" ref="wrapper">
+    <div class="scroll-content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import BetterScroll from "better-scroll";
+export default {
+  name: 'DetailScrollWrapper'
+}
+</script>
