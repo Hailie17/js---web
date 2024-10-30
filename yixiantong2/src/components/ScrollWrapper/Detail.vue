15a8478d2c96bbd85d8391d729c702fbadba22f5
Hailie17 <192151452@qq.com>
2024-10-30 23:58:16 +0800

    382 实例化

--- yixiantong2/src/components/ScrollWrapper/Detail.vue
+++ yixiantong2/src/components/ScrollWrapper/Detail.vue
@@ -8,10 +8,19 @@
 
 <script>
 import BetterScroll from "better-scroll";
+import {DetailModel} from "@/modules/detail";
 export default {
   name: 'DetailScrollWrapper',
   mounted(){
     this.scroll = new BetterScroll(this.$refs.wrapper)
+  },
+  methods: {
+    getDetail(field, id) {
+      const detailModel = new DetailModel()
+      detailModel.getDetail(field,id).then(res => {
+        console.log(res)
+      })
+    }
   }
 }
 </script>
