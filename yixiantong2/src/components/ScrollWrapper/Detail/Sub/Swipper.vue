907913d6bcb42046e0dee46e4be8802b5402d6ea
Hailie17 <192151452@qq.com>
2024-10-31 23:37:11 +0800

    985 轮播图组件

--- yixiantong2/src/components/ScrollWrapper/Detail/Sub/Swipper.vue
+++ yixiantong2/src/components/ScrollWrapper/Detail/Sub/Swipper.vue
@@ -32,4 +32,12 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.swiper-slide {
+  height: 2rem;
+}
+.swiper-img {
+  width: 100%;
+  height: 100%;
+}
+</style>
