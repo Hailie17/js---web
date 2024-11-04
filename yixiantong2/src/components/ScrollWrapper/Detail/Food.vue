73eabada6cab467c9f221ebae69c4c448c5e162c
Hailie17 <192151452@qq.com>
2024-11-05 01:51:45 +0800

    240 价格组件引用

--- yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
+++ yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
@@ -3,6 +3,7 @@
     <detail-name :name="name"></detail-name>
     <detail-stars :starNum="starNum" :score="score"></detail-stars>
     <detail-address :address="address"></detail-address>
+    <detail-price :price="price"></detail-price>
   </div>
 </template>
 
@@ -10,18 +11,21 @@
 import DetailName from './Sub/Name.vue'
 import DetailStars from './Sub/Stars.vue'
 import DetailAddress from './Sub/Address.vue'
+import DetailPrice from './Sub/Price.vue'
 export default {
   name: '',
   components: {
     DetailName,
     DetailStars,
-    DetailAddress
+    DetailAddress,
+    DetailPrice
   },
   props: {
     name: String,
     starNum: Number,
     score: String,
-    address: String
+    address: String,
+    price: Number
   }
 }
 </script>
