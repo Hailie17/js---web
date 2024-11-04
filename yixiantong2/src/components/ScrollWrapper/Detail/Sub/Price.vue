bbd4790ec984e179d7d4b0bb3378a35fe9dc4993
Hailie17 <192151452@qq.com>
2024-11-05 01:45:34 +0800

    240 价格组件

---
+++ yixiantong2/src/components/ScrollWrapper/Detail/Sub/Price.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="price-bd">
+    <span class="iconfont icon-price"></span>
+    <span class="price">￥{{price}}元起</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailPrice',
+  props: {
+    price: {
+      type: Number,
+      default(){
+        return 0
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
