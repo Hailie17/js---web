48eb0352d4d425ff73d3014b82a4bbb3c0e1cd9a
Hailie17 <192151452@qq.com>
2024-11-05 01:42:14 +0800

    240 详情

--- yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
+++ yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
@@ -2,22 +2,26 @@
   <div class="detail-wrapper">
     <detail-name :name="name"></detail-name>
     <detail-stars :starNum="starNum" :score="score"></detail-stars>
+    <detail-address :address="address"></detail-address>
   </div>
 </template>
 
 <script>
 import DetailName from './Sub/Name.vue'
 import DetailStars from './Sub/Stars.vue'
+import DetailAddress from './Sub/Address.vue'
 export default {
   name: '',
   components: {
     DetailName,
-    DetailStars
+    DetailStars,
+    DetailAddress
   },
   props: {
     name: String,
     starNum: Number,
-    score: String
+    score: String,
+    address: String
   }
 }
 </script>
