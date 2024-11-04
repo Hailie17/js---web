51352b6830756f6163c90f47c510ba80a13c6ac3
Hailie17 <192151452@qq.com>
2024-11-04 22:51:57 +0800

    240 name组件使用

--- yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
+++ yixiantong2/src/components/ScrollWrapper/Detail/Food.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="detail-wrapper">
-
+    <detail-name :name="name"></detail-name>
   </div>
 </template>
 
 <script>
+import DetailName from './Sub/Name.vue'
 export default {
   name: '',
   components: {
-
+    DetailName
   },
   props: {
-
+    name: String
   }
 }
 </script>
