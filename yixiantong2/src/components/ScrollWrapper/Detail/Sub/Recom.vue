5ea05521f5ce1495146adfbfc2debb996e442c02
Hailie17 <192151452@qq.com>
2024-11-05 22:18:11 +0800

    240 标题组件使用，固定传入值

---
+++ yixiantong2/src/components/ScrollWrapper/Detail/Sub/Recom.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="recom-bd">
+    <detail-title icon="icon-recom" title="推荐菜品"></detail-title>
+  </div>
+</template>
+
+<script>
+import DetailTitle from './Title.vue'
+export default {
+  name: 'DetailRecom',
+  components: {
+    DetailTitle
+  },
+  props: {
+    recom: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/styles/mixins';
+@import '~@/assets/styles/variables';
+</style>
