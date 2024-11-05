15e4660b89928171dc637ba452d2353628be34e0
Hailie17 <192151452@qq.com>
2024-11-05 22:14:19 +0800

    240 标题组件

---
+++ yixiantong2/src/components/ScrollWrapper/Detail/Sub/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="title-bd">
+    <h1>
+      <span :class="['iconfont', icon]"></span>
+      <span class="title">{{title}}</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailTitle',
+  props: {
+    icon: String,
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
