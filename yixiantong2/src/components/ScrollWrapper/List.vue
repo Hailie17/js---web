78b73103c75baba77b7e41500286a881bf0fa0e2
Hailie17 <192151452@qq.com>
2024-11-06 00:37:31 +0800

    910 list组件

---
+++ yixiantong2/src/components/ScrollWrapper/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="scroll-wrapper" ref="wrapper">
+    <div class="scroll-content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import BetterScroll from "better-scroll";
+import  {mapState} from "vuex";
+import {ListModel, listModel} from '@/modules/list'
+
+export default {
+  name: 'ListScrollWrapper',
+  components: {
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState(['cityId', 'field'])
+  },
+  mounted() {
+    this.scroll = new BetterScroll(this.$refs.wrapper)
+    this.getListDatas(this.cityId, this.field)
+  },
+  methods: {
+    getListDatas(cityId, field) {
+      const listModel = new ListModel()
+      listModel.getListDatas(cityId, field).then((res) => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
