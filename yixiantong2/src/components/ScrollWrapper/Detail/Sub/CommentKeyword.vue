0c113fa5212d699ebc19adb2180e5b738236e336
Hailie17 <192151452@qq.com>
2024-11-05 23:56:35 +0800

    122 关键词组件

---
+++ yixiantong2/src/components/ScrollWrapper/Detail/Sub/CommentKeyword.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="comment-bd">
+    <detial-title></detial-title>
+  </div>
+</template>
+
+<script>
+import DetailTitle from './Title.vue'
+export default {
+  name:'DetialCommentKeyword',
+  components: {
+    DetailTitle
+  },
+  props: {
+    commentKeyword: Array
+  }
+}
+</script>
