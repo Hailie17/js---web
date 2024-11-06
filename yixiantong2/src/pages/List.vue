3a909bb0e629e94a4b5f2e23ca7d0df1d0c03271
Hailie17 <192151452@qq.com>
2024-11-07 00:04:07 +0800

    516 标题组件

--- yixiantong2/src/pages/List.vue
+++ yixiantong2/src/pages/List.vue
@@ -8,15 +8,20 @@
 <script>
 import CommonHeader from '@/components/Header/Common.vue'
 import ListScrollWrapper from '@/components/ScrollWrapper/List.vue'
+import {mapState} from "vuex";
+import fieldDatas from '@/assets/data/fields'
 export default {
   name: 'List',
   components: {
     CommonHeader,
     ListScrollWrapper
   },
-  data(){
-    return {
-      title: '列表'
+  computed: {
+    ...mapState(['field']),
+    title(){
+      if (this.field) {
+        return fieldDatas.filter(item => item.field === this.field)[0].iconText + '列表'
+      }
     }
   }
 }
