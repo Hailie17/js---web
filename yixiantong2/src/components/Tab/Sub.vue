26fa1e99266dc6f9226f3bb2a0113660492a53d1
Hailie17 <192151452@qq.com>
2024-11-07 00:34:35 +0800

    516 城市选项卡组件

---
+++ yixiantong2/src/components/Tab/Sub.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="['tab-item',{current: cityInfo.cityId === cityInfo}]"></div>
+</template>
+
+<script>
+import {mapState} from "vuex";
+export default {
+  name: 'TabSub',
+  props: {
+    cityInfo: Object
+  },
+  computed: {
+    ...mapState(['cityId'])
+  },
+  methods: {
+
+  }
+}
+</script>
