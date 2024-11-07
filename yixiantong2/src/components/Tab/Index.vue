19b27332e22b606b7976092f970cd29b7f172c60
Hailie17 <192151452@qq.com>
2024-11-07 23:15:08 +0800

    516 城市选项卡组件

---
+++ yixiantong2/src/components/Tab/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="tab">
+    <tab-sub v-for="(item, indx) of cityDatas" :cityInfo="item" :key="indx"></tab-sub>
+  </nav>
+</template>
+
+<script>
+import TabSub from './Sub.vue'
+import cityDatas from '@/assets/data/cities'
+export default {
+  name: 'Tab',
+  components: {
+    TabSub
+  },
+  data (){
+    return {
+      cityDatas
+    }
+  }
+}
+</script>
