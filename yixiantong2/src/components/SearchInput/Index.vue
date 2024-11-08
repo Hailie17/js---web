64efe2d4c6d1643aa7c168df36befa136e3e6c94
Hailie17 <192151452@qq.com>
2024-11-08 21:44:48 +0800

    669 搜索框组件

---
+++ yixiantong2/src/components/SearchInput/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search-input">
+    <div class="input-wrapper">
+      <span class="iconfont icon-header-search"></span>
+      <input type="text" class="input" :placeholder="placeholder">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+  data(){
+    return {
+      placeholder: '美食 / 景点 / 美食 / 景点 / 美食 / 景点'
+    }
+  }
+}
+</script>
