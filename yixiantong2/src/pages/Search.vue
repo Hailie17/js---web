201a2c4461cfff6bbdb53f894e97ad242bed6324
Hailie17 <192151452@qq.com>
2024-11-09 15:18:28 +0800

    669 搜索框组件导入

--- yixiantong2/src/pages/Search.vue
+++ yixiantong2/src/pages/Search.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
     <common-header :title="title"></common-header>
+    <search-input></search-input>
   </div>
 </template>
 
 <script>
 import CommonHeader from '@/components/Header/Common.vue'
+import SearchInput from '@/components/SearchInput/Index.vue'
 export default {
   name: 'Search',
   components: {
-    CommonHeader
+    CommonHeader, SearchInput
   },
   data(){
     return {
