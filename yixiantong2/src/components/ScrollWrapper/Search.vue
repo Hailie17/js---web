6f3303200a4f0501e10eecfba6b740f57150fc57
Hailie17 <192151452@qq.com>
2024-11-12 23:39:36 +0800

    191 搜索组件

---
+++ yixiantong2/src/components/ScrollWrapper/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scroll-wrapper" ref="wrapper">
+    <div class="scroll-content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import BetterScroll from "better-scroll";
+
+export default {
+  name: 'SearchScrollWrapper',
+  props:{
+    data: Object
+  },
+  components:{
+
+  },
+  mounted() {
+    this.betterscroll = new BetterScroll(this.$refs.wrapper)
+  }
+
+}
+</script>
