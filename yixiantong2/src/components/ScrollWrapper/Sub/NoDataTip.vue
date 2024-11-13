65ec4a77883258d664116cfab0cbebbc7f16e14c
Hailie17 <192151452@qq.com>
2024-11-13 22:11:14 +0800

    191 没搜到信息组件

---
+++ yixiantong2/src/components/ScrollWrapper/Sub/NoDataTip.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="no-data-tip">
+    <span class="text">{{text}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text: ' - 没搜到相关信息 - '
+    }
+  }
+}
+</script>
