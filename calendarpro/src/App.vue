517a76562a5b63dca9d466005542e0cf187fae3e
Hailie17 <192151452@qq.com>
2024-12-23 01:26:57 +0800

    调用store

--- calendarpro/src/App.vue
+++ calendarpro/src/App.vue
@@ -7,11 +7,19 @@
 
 <script>
 import MyHeader from '@/components/Header'
+import { useStore } from 'vuex';
+import {computed} from 'vue'
+
 
 export default {
   name: 'App',
   components: {
     MyHeader
+  },
+  setup(){
+    const store = useStore(),
+          state = store.state
+    return computed(() => state).value // {}
   }
 }
 </script>
