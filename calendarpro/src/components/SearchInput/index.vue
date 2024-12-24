d9016313714c0339f6a521f929799d98afc7441e
Hailie17 <192151452@qq.com>
2024-12-24 20:42:21 +0800

    143  样式

--- calendarpro/src/components/SearchInput/index.vue
+++ calendarpro/src/components/SearchInput/index.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    
+  <div class="search-wrap">
+    <input type="text" :placeholder="placeholder" :maxlength="maxlength" :value="inputValue" @input="searchData($event)">
   </div>
 </template>
 
 <script>
-
-
 
 export default {
   name: 'SearchInput',
@@ -19,7 +17,31 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.search-wrap {
+  position: fixed;
+  top: .44rem;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: .38rem;
+  padding: .03rem .1rem;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+  background-color: #fff;
+  input {
+    width: 100%;
+    height: 100%;
+    font-size: .14rem;
+    border: 1px solid #ddd;
+    text-indent: .1rem;
+    border-radius: .03rem;
+    &:focus {
+      border-color: #ed4040;
+      box-shadow: 0 0 .02rem #ed4040;
+      transition: all .3s;
+    }
+  }
+}
 
 </style>
