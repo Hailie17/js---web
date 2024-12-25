4a30f9809a8e9a56348002b420a5cc6758f0d261
Hailie17 <192151452@qq.com>
2024-12-26 00:13:56 +0800

    143 路由和组件联动

--- calendarpro/src/App.vue
+++ calendarpro/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <my-header>{{headTitle}}</my-header>
+    <search-input :placeholder="placeholder" :maxlength="maxlength" />
     <router-view/>
     <tab />
   </div>
@@ -9,6 +10,7 @@
 <script>
 import MyHeader from '@/components/Header'
 import Tab from '@/components/Tab'
+import SearchInput from '@/components/SearchInput'
 import {useRouter} from 'vue-router'
 import { useStore } from 'vuex';
 import {computed, watch} from 'vue'
@@ -18,6 +20,7 @@ export default {
   name: 'App',
   components: {
     MyHeader,
+    SearchInput,
     Tab
   },
   setup(){
@@ -29,6 +32,9 @@ export default {
       return router.currentRoute.value.name
     }, (v) => {
       store.commit('setHeadTitle', v)
+      store.commit('setMaxLength', v)
+      store.commit('setPlaceholder', v)
+
     })
     return computed(() => state).value // {}
   }
