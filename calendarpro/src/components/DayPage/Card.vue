bd8d05dbce71619ae1162c80840f1ae380c624ae
Hailie17 <192151452@qq.com>
2024-12-29 22:58:53 +0800

    180 样式

--- calendarpro/src/components/DayPage/Card.vue
+++ calendarpro/src/components/DayPage/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-wraper">
+  <div class="card-wrapper">
     <div class="card">
       <div class="header">
         <span class="weekday">{{ data.weekday }}</span>
@@ -20,5 +20,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.card-wrapper {
+  padding: .15rem;
+  box-sizing: border-box;
+  .card {
+    background: url('~@/assets/img/bg.jpg') no-repeat center/cover;
+    border-radius: .15rem;
+    overflow: hidden;
+    border: 1px solid #ddd;
+    color: #fff;
+    .header {
+      height: .35rem;
+      padding: .1rem;
+      box-sizing: border-box;
+      font-size: .14rem;
+      background-color: rgba(0, 0, 0, .3);
+    }
+    .text {
+      text-align: center;
+      margin: .3rem 0;
+      text-shadow: .02rem .05rem .05rem #666;
+      &.lunar {
+        color: #f75555;
+        font-size: .5rem;
+        font-weight: bold;
+      }
+      &.date {
+        font-size: .2rem;
+      }
+    }
+  }
+}
 </style>
