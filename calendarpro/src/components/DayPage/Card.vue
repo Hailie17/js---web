34f0efec597810a5cd23e10b73ff11690c54c262
Hailie17 <192151452@qq.com>
2024-12-29 22:51:12 +0800

    180 信息卡片

---
+++ calendarpro/src/components/DayPage/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-wraper">
+    <div class="card">
+      <div class="header">
+        <span class="weekday">{{ data.weekday }}</span>
+      </div>
+      <h1 class="text lunar">{{ data.lunar }}</h1>
+      <h2 class="text date">{{ data.date }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DayCard',
+  props : {
+    data: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
