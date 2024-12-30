16d31903a78ee1695f32da63bc7162b9e13f3f59
Hailie17 <192151452@qq.com>
2024-12-30 23:56:02 +0800

    111

--- calendarpro/src/services/index.js
+++ calendarpro/src/services/index.js
@@ -1,3 +1,4 @@
+import { formatChsDate } from "@/libs/utils";
 import { getDaydata, getMonthdata, getYeardata } from "./request";
 
 export default async (store, feild, date) => {
@@ -16,7 +17,19 @@ export default async (store, feild, date) => {
   }
 
   if(data.error_code !== 0) {
-    
+    store.commit('setErrorCode', data.error_code);
+    return
+  }
+
+  let res = null
+  switch(field) {
+    case 'day':
+      res = data.result.data
+      res.date = formatChsDate(res.date, 'day')
+      res['year-month'] = formatChsDate(res['year-month', 'month'])
+      break;
+    case 'month':
+      res = data.result.data.holiday_array
   }
 
   return data
