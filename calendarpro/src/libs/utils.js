cc3586caee09e833e3e281873f5e8544fd6f92bb
Hailie17 <192151452@qq.com>
2025-01-05 15:11:10 +0800

    日期格式方法

--- calendarpro/src/libs/utils.js
+++ calendarpro/src/libs/utils.js
@@ -38,6 +38,24 @@ function mapForChsDate(date, key) {
   })
 }
 
+function getNowDate(field){
+  const date = new Date()
+
+  let year = date.getFullYear(),
+      month = date.getMonth() + 1,
+      day = date.getDate()
+  switch(field) {
+    case 'day':
+      return `${year}-${month}-${day}`
+    case 'month':
+      return `${year}-${month}`
+    case 'year':
+      return `${year}`
+    default: 
+      return `${year}-${month}-${day}`
+  }
+}
+
 export {
-  getIconData, mapForChsDate, formatChsDate
+  getIconData, mapForChsDate, formatChsDate, getNowDate
 }
