a98dddd0d3d47c6164b7d29d99fd2368cba08115
Hailie17 <192151452@qq.com>
2025-01-09 01:39:30 +0800

    解决require找不到模块的问题

--- calendarpro/src/views/Month.vue
+++ calendarpro/src/views/Month.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <error-tip v-if="errorCode" />
     <div v-else>
-      
+      <card-list :data="monthData"></card-list>
     </div>
   </div>
 </template>
@@ -12,17 +12,24 @@ import getDay from '@/services'
 import { onMounted, computed, watch } from 'vue'
 import {useStore} from 'vuex'
 import {getNowDate} from '@/libs/utils'
+import CardList from '@/components/MonthPage/List/index.vue'
 
 export default {
   name: 'MonthPage',
+  components: { CardList},
   setup() {
     const store = useStore(),
           state = store.state
     onMounted(() => {
       getDay(store, 'month', getNowDate('month'))
     })
+    watch(() => {
+      return state.monthData
+    }, () => {
+      store.commit('setErrorCode', 0)
+    })
     return {
-      dayData: computed(() => state.dayData),
+      monthData: computed(() => state.monthData),
       errorCode: computed(() => store.state.errorCode)
     }
   }
