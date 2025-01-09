4ec72044635977318034f2e46420761205bf33d4
Hailie17 <192151452@qq.com>
2025-01-09 22:38:00 +0800

    年页面实现

--- calendarpro/src/views/Year.vue
+++ calendarpro/src/views/Year.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="container">year page</div>
+  <div class="container">
+    <error-tip v-if="errorCode" />
+    <div v-else>
+      <card-list :data="yearData"></card-list>
+    </div>
+  </div>
 </template>
 
 <script>
 import getDay from '@/services'
-import { onMounted } from 'vue'
+import {onMounted, computed, watch} from 'vue'
+import {useStore} from 'vuex'
+import {getNowDate} from '@/libs/utils'
+
+import CardList from '@/components/YearPage/List/index.vue'
+import ErrorTip from '@/components/ErrorTip'
 
 export default {
   name: 'YearPage',
+  components: { CardList},
   setup(){
+    const store = useStore(),
+          state = store.state
     onMounted(()=> {
-      getDay('year', '2024')
+      getDay(store, 'year', getNowDate('year'))
     })
+    // 当yearData改变时，将错误码置为 0
+    watch(() => {
+      return state.yearData
+    }, () => {
+      store.commit('setErrorCode', 0)
+    })
+    return {
+      yearData: computed(() => state.yearData),
+      errorCode: computed(() => store.state.errorCode)
+    }
   }
 }
 </script>
