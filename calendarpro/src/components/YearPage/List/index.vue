56175133f83315ddbbf1da742d4a576925e57467
Hailie17 <192151452@qq.com>
2025-01-09 21:37:42 +0800

    345 year list组件页面

---
+++ calendarpro/src/components/YearPage/List/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list-wrapper">
+    <ul class="holiday-list">
+      <card-item v-for="(item, index) of data" :key="index" :item="item"></card-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CardItem from './Item.vue'
+export default {
+  name: 'CardList',
+  props: {
+    data: Array
+  },
+  components: {
+    CardItem
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
