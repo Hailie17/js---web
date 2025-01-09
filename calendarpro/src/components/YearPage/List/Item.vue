f2ef6883201f904d861ccfdda78724bc5ec410dc
Hailie17 <192151452@qq.com>
2025-01-09 21:44:45 +0800

    列表项组件

---
+++ calendarpro/src/components/YearPage/List/Item.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="card-item">
+    <div class="card">
+      <div class="img-wrap">
+        <img :src="require(`../../../assets/images/${item.name}.jpg`)" :alt="item.name">
+        <div class="mask">
+          <h1>{{ item.name }}</h1>
+          <h2>{{ item.startday }}</h2>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CardItem',
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
